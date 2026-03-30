c1ccebe2830298bbb943cf941bbe0c213bea121d
Tim Tyrrell <tim@tyrrell.io>
2026-03-30 17:44:14 -0600

    checkpoint

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    branch = 'master',
+    branch = 'main',
     lazy = false,
     build = ':TSUpdate',
     dependencies = {
@@ -25,72 +25,66 @@ return {
       'windwp/nvim-ts-autotag',
     },
     config = function()
-      require('nvim-treesitter.configs').setup {
-        auto_install = true,
-        ensure_installed = {
-          'bash', 'css', 'embedded_template', 'graphql', 'html', 'http',
-          'javascript', 'jsdoc', 'json', 'jsonc', 'lua', 'regex', 'ruby',
-          'tsx', 'typescript', 'vimdoc',
-        },
-        ignore_install = { 'phpdoc', 'comment' },
-        endwise = { enable = true },
-        highlight = { enable = true },
-        indent = { enable = true },
-        matchup = { enable = true },
-        autotag = { enable = true },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ['ac'] = '@class.outer',
-              ['ic'] = '@class.inner',
-              ['af'] = '@function.outer',
-              ['if'] = '@function.inner',
-              ['aS'] = '@statement.outer',
-              ['iS'] = '@statement.inner',
-              ['aC'] = '@conditional.outer',
-              ['iC'] = '@conditional.inner',
-              ['aL'] = '@loop.outer',
-              ['iL'] = '@loop.inner',
-              ['am'] = '@call.outer',
-              ['im'] = '@call.inner',
-              ['iP'] = '@parameter.inner',
-              ['aP'] = '@parameter.outer',
-            },
-            selection_modes = {
-              ['@parameter.outer'] = 'v',
-              ['@function.outer'] = 'V',
-              ['@class.outer'] = '<c-v>',
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = {
-              [']m'] = '@function.outer',
-              [']]'] = '@class.outer',
-            },
-            goto_next_end = {
-              [']M'] = '@function.outer',
-              [']['] = '@class.outer',
-            },
-            goto_previous_start = {
-              ['[m'] = '@function.outer',
-              ['[['] = '@class.outer',
-            },
-            goto_previous_end = {
-              ['[M'] = '@function.outer',
-              ['[]'] = '@class.outer',
-            },
+      -- main branch: nvim-treesitter is a parser/query manager only
+      -- highlight + indent are built-in on nvim 0.12
+      -- parsers: install via :TSInstall
+
+      local select = require('nvim-treesitter-textobjects.select')
+      local move = require('nvim-treesitter-textobjects.move')
+
+      require('nvim-treesitter-textobjects').setup {
+        select = {
+          lookahead = true,
+          selection_modes = {
+            ['@parameter.outer'] = 'v',
+            ['@function.outer'] = 'V',
+            ['@class.outer'] = '<c-v>',
           },
         },
-        query_linter = {
-          enable = false,
-          use_virtual_text = true,
-          lint_events = { 'BufWrite', 'CursorHold' },
+        move = {
+          set_jumps = true,
         },
       }
+
+      -- select keymaps
+      local select_maps = {
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['aS'] = '@statement.outer',
+        ['iS'] = '@statement.inner',
+        ['aC'] = '@conditional.outer',
+        ['iC'] = '@conditional.inner',
+        ['aL'] = '@loop.outer',
+        ['iL'] = '@loop.inner',
+        ['am'] = '@call.outer',
+        ['im'] = '@call.inner',
+        ['iP'] = '@parameter.inner',
+        ['aP'] = '@parameter.outer',
+      }
+      for key, query in pairs(select_maps) do
+        vim.keymap.set({ 'x', 'o' }, key, function()
+          select.select_textobject(query, 'textobjects')
+        end)
+      end
+
+      -- move keymaps
+      local move_maps = {
+        { ']m', move.goto_next_start, '@function.outer' },
+        { ']]', move.goto_next_start, '@class.outer' },
+        { ']M', move.goto_next_end, '@function.outer' },
+        { '][', move.goto_next_end, '@class.outer' },
+        { '[m', move.goto_previous_start, '@function.outer' },
+        { '[[', move.goto_previous_start, '@class.outer' },
+        { '[M', move.goto_previous_end, '@function.outer' },
+        { '[]', move.goto_previous_end, '@class.outer' },
+      }
+      for _, map in ipairs(move_maps) do
+        vim.keymap.set({ 'n', 'x', 'o' }, map[1], function()
+          map[2](map[3], 'textobjects')
+        end)
+      end
     end,
   },
 }
